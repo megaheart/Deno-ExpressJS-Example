5279db141c9262b5359c3341fd4fb3907cab98e8
Tran Phuc Manh Linh <linh2501@outlook.com.vn>
2023-05-24 18:54:25 +0700

    :) config deno project workspace

--- main.ts
+++ main.ts
@@ -4,7 +4,13 @@ import express from "npm:express@4.18.2";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Dinosaur API!");
+  res.send("Welcome to the Dinosaur API !");
+});
+
+app.get("/linhdeptrai", (req, res) => {
+  res.send("Deno with Linh đẹp trai vcl!");
 });
 
 app.listen(8000);
+
+console.log("Server listening on port 8000");
